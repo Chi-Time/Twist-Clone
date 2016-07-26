a5e50b3bccc01bcb4705b0d135ff757bad79844c
Achromic <music1384@yahoo.co.uk>
2016-07-26 11:55:36 +0100

    Changed player class to support better jump system and the world can now only be moved when the player is on a platform.

--- Twist Clone/Assets/Code/Scripts/Player.cs
+++ Twist Clone/Assets/Code/Scripts/Player.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	[SerializeField] private float m_JumpHeight = 0.0f;
+	[SerializeField] private float m_Gravity = 9.81f;
 	[SerializeField] private GameObject m_WorldPrefab = null;
 
-	private bool m_HasMoved = false;
-	private bool m_JumpPressed = false;
+	private bool m_CanMove = true;
+	private Quaternion CurrentRotation = Quaternion.identity;
 	private Rigidbody m_Rigidbody = null;
 
 	void Start ()
@@ -27,49 +28,59 @@ public class Player : MonoBehaviour
 
 	void CheckMovement ()
 	{
-		if(!m_HasMoved)
+		if(m_CanMove)
 		{
-			// Move right. 
-			if (Input.GetAxis ("Horizontal") > 0)
+			if(Input.GetKeyDown (KeyCode.D))
 			{
-				Move (new Vector3 (0, 0, -45));
-				m_HasMoved = true;
-				if(!m_JumpPressed)
-				{
-					Jump ();
-					m_JumpPressed = true;
-				}
+				m_WorldPrefab.transform.rotation = CurrentRotation;
+				StartCoroutine (Rotate (new Vector3(0.0f, 0.0f, -45f), .35f));
+				Jump ();
 			}
-
-			// Move left.
-			else if (Input.GetAxis ("Horizontal") < 0)
+			else if (Input.GetKeyDown ((KeyCode.A)))
 			{
-				Move (new Vector3 (0, 0, 45));
-				m_HasMoved = true;
-				if(!m_JumpPressed)
-				{
-					Jump ();
-					m_JumpPressed = true;
-				}
+				m_WorldPrefab.transform.rotation = CurrentRotation;
+				StartCoroutine (Rotate (new Vector3(0.0f, 0.0f, 45f), .35f));
+				Jump ();
 			}
-		}
-
-		if (Input.GetAxis ("Horizontal") == 0)
-		{
-			m_HasMoved = false;
-			m_JumpPressed = false;
 		}
 	}
 
-	void Move (Vector3 direction)
+	IEnumerator Rotate (Vector3 direction, float time)
 	{
-		var currentRotation = m_WorldPrefab.transform.rotation.eulerAngles;
-		direction += currentRotation;
+		m_CanMove = false;
+		float elapsedTime = 0.0f;
+
+		Quaternion startingRotation = m_WorldPrefab.transform.rotation; // have a startingRotation as well
+
+		direction.x += Mathf.Floor (startingRotation.eulerAngles.x);
+		direction.y += Mathf.Floor (startingRotation.eulerAngles.y);
+		direction.z += Mathf.Floor (startingRotation.eulerAngles.z);
+
+		CurrentRotation = Quaternion.Euler (direction);
+
+		Quaternion targetRotation =  Quaternion.Euler (direction);
+
+		while (elapsedTime < time) 
+		{
+			elapsedTime += Time.deltaTime;
+			// Rotations
+			m_WorldPrefab.transform.rotation = Quaternion.Slerp (startingRotation, targetRotation, (elapsedTime / time));
+			yield return new WaitForEndOfFrame ();
+		}
+
 		m_WorldPrefab.transform.rotation = Quaternion.Euler (direction);
+
+		yield return 0;
 	}
 
 	void Jump ()
 	{
-		m_Rigidbody.AddForce (new Vector3(0.0f, m_JumpHeight, 0.0f), ForceMode.Impulse);
+		transform.position = new Vector3 (transform.position.x, transform.position.y + m_Gravity * m_JumpHeight, transform.position.z);
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if(other.gameObject.CompareTag ("Platform"))
+			m_CanMove = true;
 	}
 }
