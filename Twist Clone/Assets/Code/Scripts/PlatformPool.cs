03024785935a27c96e9eb871d328025d23b8b3ef
Achromic <music1384@yahoo.co.uk>
2016-07-26 12:10:55 +0100

    Created a pool object to manage the handling of pool objects.

---
+++ Twist Clone/Assets/Code/Scripts/PlatformPool.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlatformPool
+{
+	[SerializeField] private List<GameObject> ActivePlatforms = new List<GameObject> ();
+	[SerializeField] private List<GameObject> InactivePlatforms = new List<GameObject> ();
+
+	public GameObject RemoveFromPool ()
+	{
+		if(ActivePlatforms.Count > 0)
+		{
+			var temp = InactivePlatforms [0];
+			ActivePlatforms.Add (temp);
+			InactivePlatforms.Remove (temp);
+
+			return temp;
+		}
+
+		return null;
+	}
+
+	public void ReturnToPool (GameObject go)
+	{
+		ActivePlatforms.Remove (go);
+		InactivePlatforms.Add (go);
+	}
+}
