0549cc9508c6c4234949a53935b1b683111903cc
Achromic <music1384@yahoo.co.uk>
2016-07-26 12:26:03 +0100

    Created a platform object which responsible for returning itself to the pool upon it's death.

---
+++ Twist Clone/Assets/Code/Scripts/Platform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour
+{
+	private PlatformPool m_Pool = null;
+
+	public void AssignReferences (PlatformPool pool)
+	{
+		m_Pool = pool;
+	}
+
+	void OnDisable ()
+	{
+		m_Pool.ReturnToPool (this.gameObject);
+	}
+}
