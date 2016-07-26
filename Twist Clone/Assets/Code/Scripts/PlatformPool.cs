7391464e504c75f03b1bd4391b8fb7f9f08431c4
Achromic <music1384@yahoo.co.uk>
2016-07-26 12:25:25 +0100

    Changed platform pool to deal with Platform objects rather than GameObjects. Also added a new method to add to the pool.

--- Twist Clone/Assets/Code/Scripts/PlatformPool.cs
+++ Twist Clone/Assets/Code/Scripts/PlatformPool.cs
@@ -5,8 +5,14 @@ using System.Collections.Generic;
 [System.Serializable]
 public class PlatformPool
 {
-	[SerializeField] private List<GameObject> ActivePlatforms = new List<GameObject> ();
-	[SerializeField] private List<GameObject> InactivePlatforms = new List<GameObject> ();
+	[SerializeField] private List<Platform> ActivePlatforms = new List<Platform> ();
+	[SerializeField] private List<Platform> InactivePlatforms = new List<Platform> ();
+
+	public void AddToPool (Platform platform)
+	{
+		platform.AssignReferences (this);
+		InactivePlatforms.Add (platform);
+	}
 
 	public GameObject RemoveFromPool ()
 	{
@@ -15,16 +21,18 @@ public class PlatformPool
 			var temp = InactivePlatforms [0];
 			ActivePlatforms.Add (temp);
 			InactivePlatforms.Remove (temp);
+			temp.gameObject.SetActive (true);
 
-			return temp;
+			return temp.gameObject;
 		}
 
 		return null;
 	}
 
-	public void ReturnToPool (GameObject go)
+	public void ReturnToPool (Platform platform)
 	{
-		ActivePlatforms.Remove (go);
-		InactivePlatforms.Add (go);
+		ActivePlatforms.Remove (platform);
+		InactivePlatforms.Add (platform);
+		platform.gameObject.SetActive (false);
 	}
 }
