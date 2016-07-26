6e53a9a39f056b2450e44c931d14c46518176ad2
Achromic <music1384@yahoo.co.uk>
2016-07-26 21:33:59 +0100

    Added functionality to platform so that it can now move and disable itself when it goes offscreen.

--- Twist Clone/Assets/Code/Scripts/Platform.cs
+++ Twist Clone/Assets/Code/Scripts/Platform.cs
@@ -3,15 +3,34 @@ using System.Collections;
 
 public class Platform : MonoBehaviour
 {
+	[Tooltip("How fast the platform should move.")]
+	[SerializeField] private float m_Speed = 5.0f;
+
+	/// Reference to the platform pool class.
 	private PlatformPool m_Pool = null;
 
+	/// Assigns the references for this class.
 	public void AssignReferences (PlatformPool pool)
 	{
 		m_Pool = pool;
 	}
 
-	void OnDisable ()
+	void Update ()
 	{
-		m_Pool.ReturnToPool (this);
+		Move ();
+		CheckPosition ();
+	}
+		
+	/// Moves the platform towards the screen.
+	void Move ()
+	{
+		transform.Translate (Vector3.back * m_Speed * Time.deltaTime);
+	}
+
+	/// Checks the platforms current position.
+	void CheckPosition ()
+	{
+		if (transform.position.z <= -10f)
+			m_Pool.ReturnToPool (this);
 	}
 }
