c6fe56deff064fda0befd3bda9148340c514cf9a
Achromic <music1384@yahoo.co.uk>
2016-07-26 12:39:49 +0100

    Changed the platform generator to class to become a serializable class. Update class to reflect this change.

--- Twist Clone/Assets/Code/Scripts/PlatformGenerator.cs
+++ Twist Clone/Assets/Code/Scripts/PlatformGenerator.cs
@@ -2,25 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlatformGenerator : MonoBehaviour
+[System.Serializable]
+public class PlatformGenerator
 {
 	[SerializeField] private int m_Amount = 10;
 	[SerializeField] private GameObject m_PlatformPrefab = null;
-	[SerializeField] private PlatformPool m_Pool = new PlatformPool();
 
-	void Awake ()
-	{
-		GeneratePool ();
-	}
-
-	void GeneratePool ()
+	public void GeneratePool (PlatformPool pool)
 	{
 		for(int i = 0; i < m_Amount; i++)
 		{
-			var temp = (GameObject)Instantiate (m_PlatformPrefab, transform.position, Quaternion.identity);
+			var temp = (GameObject)MonoBehaviour.Instantiate (m_PlatformPrefab, Vector3.zero, Quaternion.identity);
 			temp.SetActive (false);
 			temp.transform.SetParent (GameObject.Find ("World").transform);
-			m_Pool.AddToPool (temp.GetComponent <Platform> ());
+			pool.AddToPool (temp.GetComponent <Platform> ());
 		}
 	}
 }
