cc81cc86ea70548f926ee3494d1a2a3c6888aad9
Achromic <music1384@yahoo.co.uk>
2016-07-26 20:32:41 +0100

    Refactored parts of the code and provided some simple documentation.

--- Twist Clone/Assets/Code/Scripts/PlatformGenerator.cs
+++ Twist Clone/Assets/Code/Scripts/PlatformGenerator.cs
@@ -5,17 +5,24 @@ using System.Collections.Generic;
 [System.Serializable]
 public class PlatformGenerator
 {
-	[SerializeField] private int m_Amount = 10;
+	[Tooltip("The amount of platforms to spawn in the pool.")]
+	[SerializeField] private int m_PoolSize = 10;
+	[Tooltip("The platform prefab to spawn.")]
 	[SerializeField] private GameObject m_PlatformPrefab = null;
 
+	/// Generates all the pool objects, names them, de-activates them and adds them to the pool.
 	public void GeneratePool (PlatformPool pool)
 	{
-		for(int i = 0; i < m_Amount; i++)
+		for(int i = 0; i < m_PoolSize; i++)
 		{
 			var temp = (GameObject)MonoBehaviour.Instantiate (m_PlatformPrefab, Vector3.zero, Quaternion.identity);
+
 			temp.SetActive (false);
+			temp.name = "Handler: " + i;
 			temp.transform.SetParent (GameObject.Find ("World").transform);
-			pool.AddToPool (temp.GetComponent <Platform> ());
+
+			var platform = temp.GetComponent <Platform> ();
+			pool.AddToPool (platform);
 		}
 	}
 }
