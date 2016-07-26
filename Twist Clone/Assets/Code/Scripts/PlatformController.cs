c7ed90d0a23688e5f88a39adaec813fa4b3ef0fc
Achromic <music1384@yahoo.co.uk>
2016-07-26 12:38:54 +0100

    Created a new controller for the platform system. It now contains two serializable classes of both the platform generator and the platform pool.

---
+++ Twist Clone/Assets/Code/Scripts/PlatformController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour
+{
+	[SerializeField] private PlatformGenerator m_Generator = new PlatformGenerator();
+	[SerializeField] private PlatformPool m_Pool = new PlatformPool();
+
+	void Awake ()
+	{
+		m_Generator.GeneratePool (m_Pool);
+	}
+
+	void Start ()
+	{
+		
+	}
+
+	IEnumerator SpawnPlatform ()
+	{
+		
+	}
+}
