092f5175067901488e26166e4b9a5a994c014f21
Achromic <music1384@yahoo.co.uk>
2016-07-26 12:24:31 +0100

    Added platform generation functionality.

--- Twist Clone/Assets/Code/Scripts/PlatformGenerator.cs
+++ Twist Clone/Assets/Code/Scripts/PlatformGenerator.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlatformGenerator : MonoBehaviour
 {
-	
+	[SerializeField] private int m_Amount = 10;
+	[SerializeField] private GameObject m_PlatformPrefab = null;
+	[SerializeField] private PlatformPool m_Pool = new PlatformPool();
+
+	void Awake ()
+	{
+		GeneratePool ();
+	}
+
+	void GeneratePool ()
+	{
+		for(int i = 0; i < m_Amount; i++)
+		{
+			var temp = (GameObject)Instantiate (m_PlatformPrefab, transform.position, Quaternion.identity);
+			temp.SetActive (false);
+			temp.transform.SetParent (GameObject.Find ("World").transform);
+			m_Pool.AddToPool (temp.GetComponent <Platform> ());
+		}
+	}
 }
