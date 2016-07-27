22946d0a8a1f22ea557ac003c7627bd0a0ee1488
[Guardevoir] <music1384@yahoo.co.uk>
2016-07-28 00:47:29 +0100

    Added in global variable to ensure that the world always ends up at the correct position even if interrupted between Slerps.

--- Twist Clone/Assets/Code/Scripts/WorldController.cs
+++ Twist Clone/Assets/Code/Scripts/WorldController.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class WorldController : MonoBehaviour
 {
+	/// The desired rotation angle of the world.
+	private Quaternion m_TargetRotation = Quaternion.identity;
+
 	/// Rotate's the object to the position given over the amount of time passed in.
 	public IEnumerator Rotate (Vector3 direction, float time)
 	{
+		// Ensure that the world is set at the desired rotation in case the Slerp is interrupted.
+		transform.rotation = m_TargetRotation;
+
 		// The amount of time that has elapsed since the coroutine began.
 		float elapsedTime = 0.0f;
 		// The current starting rotation of the object.
@@ -17,16 +23,16 @@ public class WorldController : MonoBehaviour
 		direction.z += Mathf.Floor (startingRotation.eulerAngles.z);
 
 		// Assign this new floored vector to a Quaternion.
-		Quaternion targetRotation = Quaternion.Euler (direction);
+		m_TargetRotation = Quaternion.Euler (direction);
 
 		// Whilst the elapsed time is less than the length of the coroutine.
-		while (elapsedTime < time) 
+		while (elapsedTime < time)
 		{
 			// Increase the elapsed time.
 			elapsedTime += Time.deltaTime;
 			// Slerp the objects rotation between the starting rotation 
 			// and the target rotation to go to over the amount of elapsed / by the length of the coroutine.
-			transform.rotation = Quaternion.Slerp (startingRotation, targetRotation, (elapsedTime / time));
+			transform.rotation = Quaternion.Slerp (startingRotation, m_TargetRotation, (elapsedTime / time));
 			// Wait for the end of the frame before repeating.
 			yield return new WaitForEndOfFrame ();
 		}
