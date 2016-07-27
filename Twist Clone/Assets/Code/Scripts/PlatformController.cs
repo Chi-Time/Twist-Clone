45aebd753971bebb56d5451277d9c1f07f33e54c
[Guardevoir] <music1384@yahoo.co.uk>
2016-07-28 00:43:49 +0100

    Changed naming convention of temporary variables for clarity.

--- Twist Clone/Assets/Code/Scripts/PlatformController.cs
+++ Twist Clone/Assets/Code/Scripts/PlatformController.cs
@@ -6,9 +6,9 @@ public class PlatformController : MonoBehaviour
 	[Tooltip("The time between platform spawns.")]
 	[SerializeField] private float m_SpawnTimer = 1.0f;
 	[Tooltip("The platform generator object.")]
-	[SerializeField] private PlatformGenerator m_Generator = new PlatformGenerator();
+	[SerializeField] private PlatformGenerator m_Generator = new PlatformGenerator ();
 	[Tooltip("The platform pool object.")]
-	[SerializeField] private PlatformPool m_Pool = new PlatformPool();
+	[SerializeField] private PlatformPool m_Pool = new PlatformPool ();
 
 	/// The last object to be spawned's position.
 	private Transform m_LastObjectPosition = null;
@@ -29,26 +29,24 @@ public class PlatformController : MonoBehaviour
 		{
 			if(i == 0)
 			{
-				var temp = m_Pool.RetrieveFromPool ();
+				var platform = m_Pool.RetrieveFromPool ();
 
 				m_LastObjectPosition = m_Pool.RetrieveFromIndexPosition (0); //ActivePlatforms [0].gameObject;
 
-				print (m_LastObjectPosition.transform.GetChild (0).localScale.z);
-
-				if(temp != null)
+				if(platform != null)
 				{
-					temp.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
+					platform.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
 				}
 			}
 			else
 			{
-				var temp = m_Pool.RetrieveFromPool ();
+				var platform = m_Pool.RetrieveFromPool ();
 
 				m_LastObjectPosition = m_Pool.RetrieveFromIndexPosition (i); //ActivePlatforms [i - 1].gameObject;
 
-				if(temp != null)
+				if(platform != null)
 				{
-					temp.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
+					platform.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
 				}
 			}
 		}
@@ -58,23 +56,23 @@ public class PlatformController : MonoBehaviour
 
 	IEnumerator SpawnPlatform ()
 	{
-		m_LastObjectPosition = m_Pool.RetrieveLastPosition (); //m_Pool.ActivePlatforms [m_Pool.ActivePlatforms.Count - 1].gameObject;
+		m_LastObjectPosition = m_Pool.RetrieveLastPosition (); //ActivePlatforms [ActivePlatforms.Count - 1];
 
-		var temp = m_Pool.RetrieveFromPool ();
+		var platform = m_Pool.RetrieveFromPool ();
 
-		if(temp != null)
+		if(platform != null)
 		{
 			int direction = Random.Range (0, 2);
 
 			if(direction == 0)
 			{
-				temp.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
-				temp.transform.rotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.rotation.eulerAngles.z + 45.0f));
+				platform.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
+				platform.transform.rotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.rotation.eulerAngles.z + 45.0f));
 			}
 			else if (direction == 1)
 			{
-				temp.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
-				temp.transform.rotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.rotation.eulerAngles.z - 45.0f));
+				platform.transform.position = new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.position.z + m_LastObjectPosition.transform.GetChild (0).localScale.z);
+				platform.transform.rotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, m_LastObjectPosition.transform.rotation.eulerAngles.z - 45.0f));
 			}
 		}
 
