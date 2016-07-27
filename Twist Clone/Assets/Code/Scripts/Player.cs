66cb8460922559f0fc051fa3ad916d97699e8b24
Achromic <music1384@yahoo.co.uk>
2016-07-27 01:32:46 +0100

    Refactored player class and added functionality for world rotation, jumping and movement.

--- Twist Clone/Assets/Code/Scripts/Player.cs
+++ Twist Clone/Assets/Code/Scripts/Player.cs
@@ -4,83 +4,98 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	[SerializeField] private float m_JumpHeight = 0.0f;
-	[SerializeField] private float m_Gravity = 9.81f;
-	[SerializeField] private GameObject m_WorldPrefab = null;
+	[SerializeField] private float m_Gravity = .1f;
+	[SerializeField] private WorldController m_WorldPrefab = null;
 
+	private float m_RightAngle = -45f;
+	private float m_LeftAngle = 45f;
 	private bool m_CanMove = true;
-	private Quaternion CurrentRotation = Quaternion.identity;
+	private Quaternion m_CurrentRotation = Quaternion.identity;
 	private Rigidbody m_Rigidbody = null;
 
-	void Start ()
+	void Awake ()
 	{
 		m_Rigidbody = GetComponent <Rigidbody> ();
 	}
 
 	void Update ()
 	{
-		PollInput ();
+		CheckMovementInput ();
 	}
 
-	void PollInput ()
-	{
-		CheckMovement ();
-	}
-
-	void CheckMovement ()
+	void CheckMovementInput ()
 	{
 		if(m_CanMove)
 		{
 			if(Input.GetKeyDown (KeyCode.D))
 			{
-				m_WorldPrefab.transform.rotation = CurrentRotation;
-				StartCoroutine (Rotate (new Vector3(0.0f, 0.0f, -45f), .35f));
-				Jump ();
+				Move (m_RightAngle);
 			}
 			else if (Input.GetKeyDown ((KeyCode.A)))
 			{
-				m_WorldPrefab.transform.rotation = CurrentRotation;
-				StartCoroutine (Rotate (new Vector3(0.0f, 0.0f, 45f), .35f));
-				Jump ();
+				Move (m_LeftAngle);
 			}
 		}
 	}
 
-	IEnumerator Rotate (Vector3 direction, float time)
+	// Move the player to the next platform.
+	void Move (float rotDirection)
 	{
+		Jump ();
+		//m_WorldPrefab.transform.rotation = m_CurrentRotation;
+		StartCoroutine (m_WorldPrefab.Rotate (Vector3.forward * rotDirection, .35f));
 		m_CanMove = false;
-		float elapsedTime = 0.0f;
-
-		Quaternion startingRotation = m_WorldPrefab.transform.rotation; // have a startingRotation as well
-
-		direction.x += Mathf.Floor (startingRotation.eulerAngles.x);
-		direction.y += Mathf.Floor (startingRotation.eulerAngles.y);
-		direction.z += Mathf.Floor (startingRotation.eulerAngles.z);
-
-		CurrentRotation = Quaternion.Euler (direction);
-
-		Quaternion targetRotation =  Quaternion.Euler (direction);
-
-		while (elapsedTime < time) 
-		{
-			elapsedTime += Time.deltaTime;
-			// Rotations
-			m_WorldPrefab.transform.rotation = Quaternion.Slerp (startingRotation, targetRotation, (elapsedTime / time));
-			yield return new WaitForEndOfFrame ();
-		}
-
-		m_WorldPrefab.transform.rotation = Quaternion.Euler (direction);
-
-		yield return 0;
 	}
 
 	void Jump ()
 	{
-		transform.position = new Vector3 (transform.position.x, transform.position.y + m_Gravity * m_JumpHeight, transform.position.z);
+		// Move the player into the air.
+		transform.Translate (Vector3.up * m_JumpHeight);
+	}
+
+	/// Rotate's the object to the position given over the amount of time passed in.
+	IEnumerator Rotate (Vector3 direction, float time)
+	{
+		// The amount of time that has elapsed since the coroutine began.
+		float elapsedTime = 0.0f;
+		// The current starting rotation of the object.
+		Quaternion startingRotation = m_WorldPrefab.transform.rotation;
+
+		// Floor the values of the starting rotation so that their is no misplaced values.
+		direction.x += Mathf.Floor (startingRotation.eulerAngles.x);
+		direction.y += Mathf.Floor (startingRotation.eulerAngles.y);
+		direction.z += Mathf.Floor (startingRotation.eulerAngles.z);
+
+		// Assign this new floored vector to a Quaternion.
+		m_CurrentRotation = Quaternion.Euler (direction);
+
+		// Whilst the elapsed time is less than the length of the coroutine.
+		while (elapsedTime < time) 
+		{
+			// Increase the elapsed time.
+			elapsedTime += Time.deltaTime;
+			// Slerp the objects rotation between the starting rotation and the target rotation to go to over the amount of elapsed / by the length of the coroutine.
+			m_WorldPrefab.transform.rotation = Quaternion.Slerp (startingRotation, m_CurrentRotation, (elapsedTime / time));
+			// Wait for the end of the frame before repeating.
+			yield return new WaitForEndOfFrame ();
+		}
+
+		// After this is complete, ensure that the object is now placed within the final destination.
+		m_WorldPrefab.transform.rotation = Quaternion.Euler (direction);
+
+		// End the coroutine.
+		yield return 0;
 	}
 
 	void OnCollisionEnter (Collision other)
 	{
 		if(other.gameObject.CompareTag ("Platform"))
 			m_CanMove = true;
+	}
+
+	void OnCollisionExit (Collision other)
+	{
+		if(other.gameObject.CompareTag ("Platform"))
+			m_CanMove = false;
 	}
 }
