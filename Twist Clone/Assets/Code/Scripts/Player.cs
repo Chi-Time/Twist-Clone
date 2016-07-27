4a56fb11b51ca1651bc834b5055854035d30e77b
Achromic <music1384@yahoo.co.uk>
2016-07-27 01:37:44 +0100

    Refactored player class and placed rotation functionality into separate world controller class. This functionality is now accessed through a reference from the player.

--- Twist Clone/Assets/Code/Scripts/Player.cs
+++ Twist Clone/Assets/Code/Scripts/Player.cs
@@ -3,19 +3,17 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField] private float m_JumpHeight = 0.0f;
-	[SerializeField] private float m_Gravity = .1f;
-	[SerializeField] private WorldController m_WorldPrefab = null;
+	[Tooltip("How high the player can jump to each platform.")]
+	[SerializeField] private float m_JumpHeight = 1.5f;
 
-	private float m_RightAngle = -45f;
-	private float m_LeftAngle = 45f;
 	private bool m_CanMove = true;
-	private Quaternion m_CurrentRotation = Quaternion.identity;
-	private Rigidbody m_Rigidbody = null;
+	private float m_LeftAngle = 45f;
+	private float m_RightAngle = -45f;
+	private WorldController m_World = null;
 
 	void Awake ()
 	{
-		m_Rigidbody = GetComponent <Rigidbody> ();
+		m_World = GameObject.Find ("World").GetComponent <WorldController> ();
 	}
 
 	void Update ()
@@ -38,12 +36,11 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	// Move the player to the next platform.
+	/// Move the player to the next platform.
 	void Move (float rotDirection)
 	{
 		Jump ();
-		//m_WorldPrefab.transform.rotation = m_CurrentRotation;
-		StartCoroutine (m_WorldPrefab.Rotate (Vector3.forward * rotDirection, .35f));
+		StartCoroutine (m_World.Rotate (Vector3.forward * rotDirection, .35f));
 		m_CanMove = false;
 	}
 
@@ -51,40 +48,6 @@ public class Player : MonoBehaviour
 	{
 		// Move the player into the air.
 		transform.Translate (Vector3.up * m_JumpHeight);
-	}
-
-	/// Rotate's the object to the position given over the amount of time passed in.
-	IEnumerator Rotate (Vector3 direction, float time)
-	{
-		// The amount of time that has elapsed since the coroutine began.
-		float elapsedTime = 0.0f;
-		// The current starting rotation of the object.
-		Quaternion startingRotation = m_WorldPrefab.transform.rotation;
-
-		// Floor the values of the starting rotation so that their is no misplaced values.
-		direction.x += Mathf.Floor (startingRotation.eulerAngles.x);
-		direction.y += Mathf.Floor (startingRotation.eulerAngles.y);
-		direction.z += Mathf.Floor (startingRotation.eulerAngles.z);
-
-		// Assign this new floored vector to a Quaternion.
-		m_CurrentRotation = Quaternion.Euler (direction);
-
-		// Whilst the elapsed time is less than the length of the coroutine.
-		while (elapsedTime < time) 
-		{
-			// Increase the elapsed time.
-			elapsedTime += Time.deltaTime;
-			// Slerp the objects rotation between the starting rotation and the target rotation to go to over the amount of elapsed / by the length of the coroutine.
-			m_WorldPrefab.transform.rotation = Quaternion.Slerp (startingRotation, m_CurrentRotation, (elapsedTime / time));
-			// Wait for the end of the frame before repeating.
-			yield return new WaitForEndOfFrame ();
-		}
-
-		// After this is complete, ensure that the object is now placed within the final destination.
-		m_WorldPrefab.transform.rotation = Quaternion.Euler (direction);
-
-		// End the coroutine.
-		yield return 0;
 	}
 
 	void OnCollisionEnter (Collision other)
---
+++ Twist Clone/Assets/Code/Scripts/WorldController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour
+{
+	/// Rotate's the object to the position given over the amount of time passed in.
+	public IEnumerator Rotate (Vector3 direction, float time)
+	{
+		// The amount of time that has elapsed since the coroutine began.
+		float elapsedTime = 0.0f;
+		// The current starting rotation of the object.
+		Quaternion startingRotation = transform.rotation;
+
+		// Floor the values of the starting rotation so that their is no misplaced values.
+		direction.x += Mathf.Floor (startingRotation.eulerAngles.x);
+		direction.y += Mathf.Floor (startingRotation.eulerAngles.y);
+		direction.z += Mathf.Floor (startingRotation.eulerAngles.z);
+
+		// Assign this new floored vector to a Quaternion.
+		Quaternion targetRotation = Quaternion.Euler (direction);
+
+		// Whilst the elapsed time is less than the length of the coroutine.
+		while (elapsedTime < time) 
+		{
+			// Increase the elapsed time.
+			elapsedTime += Time.deltaTime;
+			// Slerp the objects rotation between the starting rotation 
+			// and the target rotation to go to over the amount of elapsed / by the length of the coroutine.
+			transform.rotation = Quaternion.Slerp (startingRotation, targetRotation, (elapsedTime / time));
+			// Wait for the end of the frame before repeating.
+			yield return new WaitForEndOfFrame ();
+		}
+
+		// After this is complete, ensure that the object is now placed within the final destination.
+		transform.rotation = Quaternion.Euler (direction);
+
+		// End the coroutine.
+		yield return 0;
+	}
+}
