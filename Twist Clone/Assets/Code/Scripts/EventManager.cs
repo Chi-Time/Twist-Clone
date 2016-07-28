ce9ea15f2a03c9dd9c8038c255d9a12a2a4f8100
[Guardevoir] <music1384@yahoo.co.uk>
2016-07-28 15:29:25 +0100

    Created an event manager object to control flow of state changes amongst controllers.

---
+++ Twist Clone/Assets/Code/Scripts/EventManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void StatesHandler (States state);
+
+public static class EventManager : MonoBehaviour
+{
+	public static event StatesHandler OnStateSwitched;
+
+	public static void SwitchState (States state)
+	{
+		OnStateSwitched (state);
+	}
+}
